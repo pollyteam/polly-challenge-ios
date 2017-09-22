b21ae855d468d560fd39bad87d159b5439881aac
Vicc Alexander <viccalexander@gmail.com>
2017-09-22 14:29:59 -0700

    Initial commit

---
+++ Polly-Challenge/Etc/Models/PLYOption.swift
@@ -0,0 +1,19 @@
+//
+//  PLYOption.swift
+//  Polly-Challenge
+//
+//  Created by Vicc Alexander on 9/22/17.
+//  Copyright © 2017 Polly. All rights reserved.
+//
+
+import UIKit
+
+class PLYOption: NSObject {
+
+    //-----------------------------------
+    // MARK: - Properties
+    //-----------------------------------
+
+    var text: String?
+    var votesCount: Int = 0
+}
